82e7ee2ef1dcce0b12ad714fc619fea24f48158e
Krishna Prajapati <kp602552@gmail.com>
2026-06-10 14:21:07 +0530

    Time: 11 ms (11.28%), Space: 28 MB (5.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,31 @@
+
+#include<unordered_set>
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int expectedSum = n*(n+1) / 2;
-        int actualSum = 0;
-        for(int i = 0 ; i < nums.size() ; i++)
-        {
-            actualSum += nums[i];
-        }
-        return expectedSum-actualSum;
+        // int n = nums.size();
+        // int expectedSum = n*(n+1) / 2;
+        // int actualSum = 0;
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     actualSum += nums[i];
+        // }
+        // return expectedSum-actualSum;
+        
 
+        //USING HASH SET
+        unordered_set<int> st;
+        for(int num : nums)
+        {
+            st.insert(num);
+        }
+        for(int i = 0 ; i <= nums.size(); i++)
+        {
+            if(st.find(i) == st.end())
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };
