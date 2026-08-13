e88b5617b31839ef1e2879404b10d43d16f14451
Krishna Prajapati <kp602552@gmail.com>
2026-08-13 23:15:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,6 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target ) {
-        unordered_map<int,int> mp;
-        for(int i = 0 ; i < nums.size() ; i++ )
-        {
-            int comp = target - nums[i];
-            if(mp.find(comp) != mp.end())
-            {
-                
-                return {mp[comp],i};
-                
-            }
-            mp[nums[i]] = i;
-        } 
-        return {};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
     }
 };
