902ebe0f16b470a935d33f8ab27d92fcaa9c8a29
Krishna Prajapati <kp602552@gmail.com>
2026-09-04 11:41:45 +0530

    Time: 676 ms (62.33%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -6,7 +6,7 @@ SELECT
             order_date = customer_pref_delivery_date 
                 THEN 1
         ELSE 0 
-        END ) / COUNT(*)  ,2) 
+        END ) / COUNT( DISTINCT customer_id) ,2) 
         as immediate_percentage
 FROM 
     Delivery
